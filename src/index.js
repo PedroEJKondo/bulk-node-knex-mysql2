c61767222406bb25c7b2c7d0d34d3a9e8a55d093
Pedro Kondo <pedrokondo20@gmail.com>
2023-05-07 03:30:12 +0100

    commit dos dados actualizados

--- src/index.js
+++ src/index.js
@@ -27,11 +27,13 @@ async function migrateData(trx) {
         getDocumentos()
     ])
         .then(async results => {
+
             const [municipios, documentos] = results;
 
             for (const doc of documentos) {
 
-                let municipio = municipios.find((item) => item.id == doc.local) 
+                let municipio = municipios.find((item) => item.id == doc.local)
+
                 await knex
                     .from('documentos')
                     .transacting(trx)
@@ -44,11 +46,13 @@ async function migrateData(trx) {
             }
 
         })
-        .catch(error => {
+        .catch(async error => {
             console.error(error);
+            await trx.rollback();
         })
-        .finally(() => {
+        .finally(async () => {
             knex.destroy();
+            await trx.commit();
         });
 
 }
@@ -57,42 +61,18 @@ async function main() {
 
     const trx = await knex.transaction();
 
-    /*
-      console.time('MIGRATION');
-    
-    
-      try {
-        await Promise.resolve()
-          // .then(() => createNature(workbook, trx))
-          // .then(() => createUnit(workbook, trx))
-          // .then(() => createTemplate(workbook, trx))
-          // .then(() => createGroups(workbook, trx))
-          // .then(() => addTemplateToGroup(workbook, trx))
-          // .then(() => createOrganism(workbook, trx))
-          // .then(() => addOrganismToGroup(workbook, trx))
-          // .then(() => addSchedule(workbook, trx))
-          // .then(() => addFieldsToTemplate(workbook, trx))
-          .then(() => generateBITable(workbook, trx))
-          .then(() => console.log('done'))
-          .then(() => console.timeEnd('MIGRATION'));
-        await trx.commit();
-      } catch (err) {
-        console.log(err);
-    
-        await trx.rollback();
-      }
-    */
     try {
         await migrateData(trx)
-        await trx.rollback();
+        // await trx.rollback();
     } catch (err) {
-        await trx.rollback();
+        // await trx.rollback();
         console.log(err);
         process.exit(1);
     }
     finally {
     }
 }
+
 console.log('Fim da migração da actualização dos dados !!!')
 
 main();
